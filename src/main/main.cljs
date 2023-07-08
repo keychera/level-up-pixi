90340b3f0c73c3e8f8a30a6d61742f8435f7c50e
keychera <kyogatama@gmail.com>
2023-07-08 23:20:46 +0700

    bunny

--- src/main/main.cljs
+++ src/main/main.cljs
@@ -3,8 +3,13 @@
 (defn init []
   (println "Hello Shadow")
   (let [app (js/PIXI.Application.
-             (clj->js {:background "#1099bb" :width 640 :height 360}))]
-    (js/document.body.appendChild app.view)))
+             (clj->js {:background "#1099bb" :width 640 :height 360}))] 
+    (js/document.body.appendChild app.view)
+    (let [bunny (js/PIXI.Sprite.from "https://pixijs.com/assets/bunny.png")]
+      (.. app -stage (addChild bunny))
+      (.. bunny -anchor (set 0.5))
+      (set! (.. bunny -x) (/ (.. app -screen -width) 2))
+      (set! (.. bunny -y) (/ (.. app -screen -height) 2)))))
 
 (comment
   (init))
