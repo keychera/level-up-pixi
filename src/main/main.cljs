1cc16b81f71b5416282a3e7170f05dfffdb75496
keychera <kyogatama@gmail.com>
2023-07-08 23:53:37 +0700

    hot-reloading pixi (not efficient yet)

--- src/main/main.cljs
+++ src/main/main.cljs
@@ -1,16 +1,24 @@
 (ns main
   (:require ["pixi.js" :as PIXI]))
 
+(defonce app (atom nil))
+
+(defn ^:dev/after-load render []
+  (let [^js currently @app
+        bunny (PIXI/Sprite.from "https://pixijs.com/assets/bunny.png")]
+    (.. currently -stage (addChild bunny))
+    (.. bunny -anchor (set 0.5))
+    (set! (.. bunny -x) (/ (.. currently -screen -width) 2))
+    (set! (.. bunny -y) (/ (.. currently -screen -height) 2))))
+
 (defn init []
   (println "Hello Shadow")
-  (let [app (PIXI/Application.
-             (clj->js {:background "#1099bb" :width 640 :height 360}))] 
-    (js/document.body.appendChild app.view)
-    (let [bunny (PIXI/Sprite.from "https://pixijs.com/assets/bunny.png")]
-      (.. app -stage (addChild bunny))
-      (.. bunny -anchor (set 0.5))
-      (set! (.. bunny -x) (/ (.. app -screen -width) 2))
-      (set! (.. bunny -y) (/ (.. app -screen -height) 2)))))
+  (let [initial-app (PIXI/Application.
+                     (clj->js {:background "#1099bb" :width 640 :height 360}))]
+    (reset! app initial-app)
+    (js/document.body.appendChild initial-app.view)
+    (render)))
+
 
 (comment
   (init))
