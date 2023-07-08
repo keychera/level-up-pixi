9cb13516bbfd959014520a0d914214db800a3cf3
keychera <kyogatama@gmail.com>
2023-07-08 23:11:53 +0700

    init pixi.js

--- src/main/main.cljs
+++ src/main/main.cljs
@@ -1,7 +1,10 @@
 (ns main)
 
 (defn init []
-  (println "Hello Shadow"))
+  (println "Hello Shadow")
+  (let [app (js/PIXI.Application.
+             (clj->js {:background "#1099bb" :width 640 :height 360}))]
+    (js/document.body.appendChild app.view)))
 
 (comment
   (init))
