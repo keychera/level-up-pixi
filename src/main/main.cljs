f52308dfeb8fa4e91b1893b6f04405d815374d1a
keychera <kyogatama@gmail.com>
2023-07-08 23:33:01 +0700

    pixi.js as npm deps

--- src/main/main.cljs
+++ src/main/main.cljs
@@ -1,11 +1,12 @@
-(ns main)
+(ns main
+  (:require ["pixi.js" :as PIXI]))
 
 (defn init []
   (println "Hello Shadow")
-  (let [app (js/PIXI.Application.
+  (let [app (PIXI/Application.
              (clj->js {:background "#1099bb" :width 640 :height 360}))] 
     (js/document.body.appendChild app.view)
-    (let [bunny (js/PIXI.Sprite.from "https://pixijs.com/assets/bunny.png")]
+    (let [bunny (PIXI/Sprite.from "https://pixijs.com/assets/bunny.png")]
       (.. app -stage (addChild bunny))
       (.. bunny -anchor (set 0.5))
       (set! (.. bunny -x) (/ (.. app -screen -width) 2))
