71b91648a1da26f103b3bf02a821003d8a157d8a
keychera <kyogatama@gmail.com>
2023-07-09 14:00:47 +0700

    atomic bunny

---
+++ README.md
@@ -0,0 +1,6 @@
+# level-up with pixi.js + shadow cljs
+
+## quick run
+```sh
+npx shadow-cljs watch frontend
+```
--- src/main/main.cljs
+++ src/main/main.cljs
@@ -2,11 +2,11 @@
   (:require ["pixi.js" :as PIXI]))
 
 (defonce app (atom nil))
+(defonce bunny (atom nil))
 
 (defn ^:dev/after-load render []
   (let [^js currently @app
-        bunny (PIXI/Sprite.from "https://pixijs.com/assets/bunny.png")]
-    (.. currently -stage (addChild bunny))
+        ^js bunny @bunny]
     (.. bunny -anchor (set 0.5))
     (set! (.. bunny -x) (/ (.. currently -screen -width) 2))
     (set! (.. bunny -y) (/ (.. currently -screen -height) 2))))
@@ -14,8 +14,11 @@
 (defn init []
   (println "Hello Shadow")
   (let [initial-app (PIXI/Application.
-                     (clj->js {:background "#1099bb" :width 640 :height 360}))]
+                     (clj->js {:background "#1099bb" :width 640 :height 360}))
+        bunny-sprite (PIXI/Sprite.from "https://pixijs.com/assets/bunny.png")]
     (reset! app initial-app)
+    (reset! bunny bunny-sprite)
+    (.. initial-app -stage (addChild bunny-sprite))
     (js/document.body.appendChild initial-app.view)
     (render)))
 
