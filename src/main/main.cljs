3437d532070dc4bd1dc78583f6487d7d2e10782f
keychera <kyogatama@gmail.com>
2023-07-10 21:02:39 +0700

    prep a sprite

--- src/main/main.cljs
+++ src/main/main.cljs
@@ -1,22 +1,21 @@
 (ns main
   (:require ["pixi.js" :as PIXI]))
 
-(defn render [delta ^js app ^js bunny]
-  (.. bunny -anchor (set 0.5))
-  (set! (.. bunny -x) (/ (.. app -screen -width) 2))
-  (set! (.. bunny -y) (/ (.. app -screen -height) 2))
-  (let [rotation (.. bunny -rotation)]
-    (set! (.. bunny -rotation) (+ rotation (* 0.1 delta)))))
+(defn render [delta ^js app ^js chest] 
+  (set! (.. chest -x) (/ (.. app -screen -width) 2))
+  (set! (.. chest -y) (/ (.. app -screen -height) 2))
+  (let [rotation (.. chest -rotation)]
+    (set! (.. chest -rotation) (+ rotation (* 0.1 delta)))))
 
 (defn init []
   (println "Hello Shadow")
-  (let [app (PIXI/Application. (clj->js {:background "#1099bb" 
-                                         :resolution js/devicePixelRatio 
-                                         }))
-        bunny (PIXI/Sprite.from "https://pixijs.com/assets/bunny.png")
-        render-fn (fn [delta] (render delta app bunny))]
-    (.. app -stage (addChild bunny))
+  (let [app (PIXI/Application. (clj->js {:background "#1099bb"
+                                         :resolution js/devicePixelRatio}))
+        chest (PIXI/Sprite.from "/sprites/chest_golden_closed.png")
+        render-fn (fn [delta] (render delta app chest))]
     (js/document.body.appendChild app.view)
+    (.. app -stage (addChild chest))
+    (.. chest -anchor (set 0.5))
     (.. app -ticker (add render-fn))))
 
 
